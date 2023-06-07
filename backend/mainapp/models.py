2f0ee16b38f586649020dee5d566da9d37977ece
Adithya Dinesh <adithyadinesh33@gmail.com>
2023-06-07 17:00:49 +0530

    added new images

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField(null=True)
+    gender = models.CharField(max_length=10, choices=(
+        ('Male', 'Male'),
+        ('Female', 'Female')
+    ))
+
+class Diary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    content = models.TextField()
+
+class Emotion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    emotion = models.CharField(max_length=20)
