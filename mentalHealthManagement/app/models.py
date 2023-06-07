8df3a628e445f2817ad6483b282edec63866f82b
Adithya Dinesh <adithyadinesh33@gmail.com>
2023-06-08 00:37:11 +0530

    discarded the backend folder and restarting.

--- mentalHealthManagement/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    gender = models.CharField(max_length=10)
-    dob = models.DateField()
-    age = models.IntegerField()
